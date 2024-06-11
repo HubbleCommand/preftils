ef35d9da63818308bbaa9022afb694bb69a45f44
HubbleCommand <sasha.poirier@gmail.com>
2024-06-11 21:44:24 +0200

    java support
    + java equivalents to PreferenceUtils
    + supports Java-type serialization

---
+++ preftils/src/androidTest/java/com/hubble/preftils/InstrumentedTests.java
@@ -0,0 +1,123 @@
+package com.hubble.preftils;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+public class InstrumentedTests {
+    static SharedPreferences preferences;
+    static SharedPreferences.Editor editor;
+
+    @BeforeClass
+    public static void setup(){
+        preferences = InstrumentationRegistry.getInstrumentation().getTargetContext().getSharedPreferences("prefs", Context.MODE_PRIVATE);
+        editor = preferences.edit();
+
+        editor.remove(BasicPreferences.NUMBER.getKey());
+        editor.apply();
+        editor.commit();
+    }
+
+    static class BasicPreferences {
+        static PreferenceUtils.Preference<Integer> NUMBER = new PreferenceUtils.Preference<>("int", 10);
+    }
+
+    @Test
+    public void testJavaBasic() throws IOException, ClassNotFoundException {
+        //as intended, intellisense will show error & fails to compile
+        //PreferenceUtils.set(editor, preference, "10");
+
+        //Test read
+        Integer prefVal = PreferenceUtils.get(preferences, BasicPreferences.NUMBER);
+        assertEquals(BasicPreferences.NUMBER.getDefault(), prefVal);
+
+        //Test write
+        PreferenceUtils.put(editor, BasicPreferences.NUMBER, 10);
+    }
+
+    public static class SerializableType implements Serializable {
+        SerializableType(int integer, String string) {
+            this.integer = integer;
+            this.string = string;
+        }
+
+        public int integer;
+        public String string;
+
+        @Override
+        public boolean equals(@Nullable Object o) {
+            if (o == this) {
+                return true;
+            }
+
+            if (!(o instanceof SerializableType)) {
+                return false;
+            }
+
+            SerializableType t = (SerializableType) o;
+            return this.integer == t.integer && this.string.equals(t.string);
+        }
+
+        @NonNull
+        @Override
+        public String toString() {
+            return String.format("{integer: %d, string: %s}", this.integer, this.string);
+        }
+    }
+
+    static class SerializablePreferences {
+        static PreferenceUtils.Preference<SerializableType> SERIALIZABLE = new PreferenceUtils.Preference<>("int", new SerializableType(10, "string"));
+    }
+
+    @Test
+    public void testJavaSerialization() throws IOException, ClassNotFoundException {
+        //Test read
+        SerializableType prefVal = PreferenceUtils.get(preferences, SerializablePreferences.SERIALIZABLE);
+        assertEquals(SerializablePreferences.SERIALIZABLE.getDefault(), prefVal);
+
+        //Test write
+        int newInt = 23;
+        prefVal.integer = newInt;
+        String newString = "another string";
+        prefVal.string = newString;
+        PreferenceUtils.put(editor, SerializablePreferences.SERIALIZABLE, prefVal);
+
+        //Verify write
+        prefVal = PreferenceUtils.get(preferences, SerializablePreferences.SERIALIZABLE);
+        System.out.println(prefVal.toString());
+        assertEquals(new SerializableType(newInt, newString), prefVal);
+    }
+
+    class NonSerializableType { }
+
+    @Test
+    public void testJavaIncompatibleType() throws IOException, ClassNotFoundException {
+        //This will not compile, as NonSerializableType does not implement Serializable
+        //PreferenceUtils.Preference<NonSerializableType> INCOMPATIBLE = new PreferenceUtils.Preference<>("int", new NonSerializableType());
+
+        //Primitives supported by SharedPreferences will work as expected
+        PreferenceUtils.Preference<String> str = new PreferenceUtils.Preference<>("str", "string");
+        PreferenceUtils.Preference<Integer> nt = new PreferenceUtils.Preference<>("int", 10);
+        PreferenceUtils.Preference<Long> lng = new PreferenceUtils.Preference<>("lng", 10L);
+        PreferenceUtils.Preference<Boolean> bln = new PreferenceUtils.Preference<>("bln", true);
+        PreferenceUtils.Preference<Float> flt = new PreferenceUtils.Preference<>("flt", 10f);
+
+        assertEquals(PreferenceUtils.get(preferences, str), "string");
+        assertEquals(PreferenceUtils.get(preferences, nt), (Integer) 10);
+        assertEquals(PreferenceUtils.get(preferences, lng), (Long) 10L);
+        assertEquals(PreferenceUtils.get(preferences, bln), (Boolean) true);
+        assertEquals(PreferenceUtils.get(preferences, flt), 10f);
+    }
+}
---
+++ preftils/src/main/java/com/hubble/preftils/PreferenceUtils.java
@@ -0,0 +1,130 @@
+package com.hubble.preftils;
+
+import android.content.SharedPreferences;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class PreferenceUtils {
+    static class Preference<T extends Serializable> {
+        private final String key;
+        private final T defaultValue;
+
+        /**
+         * Wrapper class for handling SharedPreferences
+         *
+         * @param key the key for the SharedPreference
+         * @param defaultValue the default value for the SharedPreference
+         */
+        Preference(String key, @NotNull T defaultValue) {
+            this.key = key;
+            this.defaultValue = defaultValue;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public T getDefault() {
+            return defaultValue;
+        }
+
+        public T getValue(SharedPreferences preferences) throws IOException, ClassNotFoundException {
+            return get(preferences, this);
+        }
+
+        public void putValue(SharedPreferences.Editor editor, @NotNull T value) throws IOException {
+            put(editor, this, value);
+        }
+    }
+    /**
+     * Retrieves the value of a preference in a type-safe way
+     * <p>
+     * Propagates errors thrown by ObjectInputStream.readObject() if decoding is impossible
+     *
+     * @param preferences instance of SharedPreferences to read from
+     * @param preference the preference to read
+     * @return the value retrieved in SharedPreferences, or the default value of the [preference] parameter
+     *
+     * @throws  ClassNotFoundException Class of a serialized object cannot be
+     *          found.
+     * @throws  IOException Any of the usual Input/Output related exceptions.
+     *
+     * @see     ObjectInputStream#readObject()
+     */
+    @SuppressWarnings("unchecked")
+    static <T extends Serializable> T get(SharedPreferences preferences, Preference<T> preference) throws IOException, ClassNotFoundException {
+        if (preference.getDefault() instanceof Integer) {
+            return (T) (Integer) preferences.getInt(preference.getKey(), (Integer) preference.getDefault());
+        } else if (preference.getDefault() instanceof Long) {
+            return (T) (Long) preferences.getLong(preference.getKey(), (Long) preference.getDefault());
+        } else if (preference.getDefault() instanceof Float) {
+            return (T) (Float) preferences.getFloat(preference.getKey(), (Float) preference.getDefault());
+        } else if (preference.getDefault() instanceof Boolean) {
+            return (T) (Boolean) preferences.getBoolean(preference.getKey(), (Boolean) preference.getDefault());
+        } else if (preference.getDefault() instanceof String) {
+            return (T) preferences.getString(preference.getKey(), (String) preference.getDefault());
+        } else {
+            String string = preferences.getString(preference.getKey(), null);
+
+            if (string == null) {
+                return preference.getDefault();
+            }
+
+            ByteArrayInputStream inStream = new ByteArrayInputStream(string.getBytes());
+            ObjectInputStream in = new ObjectInputStream(inStream);
+
+            T object = (T) in.readObject();
+
+            inStream.close();
+            in.close();
+
+            return object;
+        }
+    }
+
+    /**
+     * Sets the value of a preference in a type-safe way
+     * <p>
+     * Propagates errors thrown by ObjectOutputStream.writeObject() if encoding is impossible
+     *
+     * @param editor Instance of SharedPreferences.Editor to write to
+     * @param preference the Preference to set
+     * @param value the value to set the Preference to
+     *
+     * @throws  IOException Any exception thrown by the underlying
+     *          OutputStream.
+     *
+     * @see     ObjectOutputStream#writeObject(Object) ()
+     */
+    static <T extends Serializable> void put(SharedPreferences.Editor editor, Preference<T> preference, @NotNull T value) throws IOException {
+        if (value instanceof Integer) {
+            editor.putInt(preference.getKey(), (Integer) value);
+        } else if (value instanceof Long) {
+            editor.putLong(preference.getKey(), (Long) value);
+        } else if (value instanceof Float) {
+            editor.putFloat(preference.getKey(), (Float) value);
+        } else if (value instanceof Boolean) {
+            editor.putBoolean(preference.getKey(), (Boolean) value);
+        } else if (value instanceof String) {
+            editor.putString(preference.getKey(), (String) value);
+        } else {
+            ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+            ObjectOutputStream out = new ObjectOutputStream(outStream);
+
+            out.writeObject(value);
+            out.flush();
+            out.close();
+
+            editor.putString(preference.getKey(), outStream.toString());
+            outStream.flush();
+            outStream.close();
+        }
+    }
+}
