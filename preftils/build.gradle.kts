6f879a6d540ad00bace395013419ae14c67208e1
HubbleCommand <sasha.poirier@gmail.com>
2024-06-11 10:14:12 +0200

    add serialization support
    + bump kotlin 1.9 -> 2.0

--- preftils/build.gradle.kts
+++ preftils/build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
     id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    kotlin("android")
+    kotlin("plugin.serialization") apply true
     id("maven-publish")
 }
 
 android {
     namespace = "com.hubble.preftils"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 24
@@ -34,10 +35,7 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
