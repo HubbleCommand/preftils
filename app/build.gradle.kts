2b950e37bc7d923fc26fdfb3785efeebd21cc28d
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2024-10-27 09:25:01 +0100

    update compile & target sdk 34 -> 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.hubble.preftilsapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.hubble.preftilsapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
